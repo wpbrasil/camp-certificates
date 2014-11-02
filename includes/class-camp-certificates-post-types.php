b81bf0d2480c7e779b5e2965cebdb8537bf16ba2
claudiosmweb <contato@claudiosmweb.com>
2014-11-01 18:01:12 -0700

    Changed the cc_events for a post type and created the cc_attendee_type taxonomy

--- includes/class-camp-certificates-post-types.php
+++ includes/class-camp-certificates-post-types.php
@@ -22,9 +22,35 @@ class Camp_Certificates_Post_Types {
 	 * @return void
 	 */
 	public function register_post_types() {
-		if ( post_type_exists( 'cc_attendees' ) ) {
+		if ( post_type_exists( 'cc_events' ) ) {
 			return;
 		}
+
+		register_post_type( 'cc_events', array(
+			'labels'          => array(
+				'name'               => _x( 'Events', 'post type general name', 'camp-certificates' ),
+				'singular_name'      => _x( 'Event', 'post type singular name', 'camp-certificates' ),
+				'menu_name'          => _x( 'Events', 'admin menu', 'camp-certificates' ),
+				'name_admin_bar'     => _x( 'Event', 'add new on admin bar', 'camp-certificates' ),
+				'add_new'            => _x( 'Add New', 'book', 'camp-certificates' ),
+				'add_new_item'       => __( 'Add New Event', 'camp-certificates' ),
+				'new_item'           => __( 'New Event', 'camp-certificates' ),
+				'edit_item'          => __( 'Edit Event', 'camp-certificates' ),
+				'view_item'          => __( 'View Event', 'camp-certificates' ),
+				'all_items'          => __( 'All Events', 'camp-certificates' ),
+				'search_items'       => __( 'Search Events', 'camp-certificates' ),
+				'parent_item_colon'  => __( 'Parent Events:', 'camp-certificates' ),
+				'not_found'          => __( 'No events found.', 'camp-certificates' ),
+				'not_found_in_trash' => __( 'No events found in Trash.', 'camp-certificates' )
+			),
+			'public'          => false,
+			'show_ui'         => true,
+			'capability_type' => 'post',
+			'hierarchical'    => false,
+			'supports'        => array( 'title', 'author' ),
+			'menu_position'   => null,
+			'menu_icon'       => 'dashicons-clipboard'
+		) );
 
 		register_post_type( 'cc_attendees', array(
 			'labels'          => array(
@@ -47,7 +73,7 @@ class Camp_Certificates_Post_Types {
 			'show_ui'         => true,
 			'capability_type' => 'post',
 			'hierarchical'    => false,
-			'supports'        => array( 'title', 'editor', 'author' ),
+			'supports'        => array( 'title', 'author' ),
 			'menu_position'   => null,
 			'menu_icon'       => 'dashicons-groups'
 		) );
@@ -57,23 +83,23 @@ class Camp_Certificates_Post_Types {
 	 * Register Taxonomies.
 	 */
 	public function register_taxonomies() {
-		if ( taxonomy_exists( 'cc_events' ) ) {
+		if ( taxonomy_exists( 'cc_attendee_type' ) ) {
 			return;
 		}
 
-		register_taxonomy( 'cc_events', 'cc_attendees', array(
+		register_taxonomy( 'cc_attendee_type', 'cc_attendees', array(
 			'labels'       => array(
-				'name'              => _x( 'Events', 'taxonomy general name', 'camp-certificates' ),
-				'singular_name'     => _x( 'Event', 'taxonomy singular name', 'camp-certificates' ),
-				'menu_name'         => _x( 'Events', 'admin menu', 'camp-certificates' ),
-				'search_items'      => __( 'Search Events', 'camp-certificates' ),
-				'all_items'         => __( 'All Events', 'camp-certificates' ),
-				'parent_item'       => __( 'Parent Events', 'camp-certificates' ),
-				'parent_item_colon' => __( 'Parent Events:', 'camp-certificates' ),
-				'edit_item'         => __( 'Edit Events', 'camp-certificates' ),
-				'update_item'       => __( 'Update Events', 'camp-certificates' ),
-				'add_new_item'      => __( 'Add New Events', 'camp-certificates' ),
-				'new_item_name'     => __( 'New Events', 'camp-certificates' )
+				'name'              => _x( 'Attendee Types', 'taxonomy general name', 'camp-certificates' ),
+				'singular_name'     => _x( 'Attendee Type', 'taxonomy singular name', 'camp-certificates' ),
+				'menu_name'         => _x( 'Attendee Types', 'admin menu', 'camp-certificates' ),
+				'search_items'      => __( 'Search Attendee Types', 'camp-certificates' ),
+				'all_items'         => __( 'All Attendee Types', 'camp-certificates' ),
+				'parent_item'       => __( 'Parent Attendee Types', 'camp-certificates' ),
+				'parent_item_colon' => __( 'Parent Attendee Types:', 'camp-certificates' ),
+				'edit_item'         => __( 'Edit Attendee Type', 'camp-certificates' ),
+				'update_item'       => __( 'Update Attendee Type', 'camp-certificates' ),
+				'add_new_item'      => __( 'Add New Attendee Type', 'camp-certificates' ),
+				'new_item_name'     => __( 'New Attendee Type', 'camp-certificates' )
 			),
 			'public'       => false,
 			'show_ui'      => true,
