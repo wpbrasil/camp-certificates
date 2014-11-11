35d78f5d78585a3e43e5c0a823b83f47231c824a
claudiosmweb <contato@claudiosmweb.com>
2014-11-10 22:47:24 -0200

    Created method for save the meta boxes

--- includes/admin/class-camp-certificates-admin-metaboxes.php
+++ includes/admin/class-camp-certificates-admin-metaboxes.php
@@ -9,10 +9,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Camp_Certificates_Admin_Metaboxes {
 
 	/**
+	 * Meta boxes nonce.
+	 *
+	 * @var string
+	 */
+	public $nonce = 'camp_certificates';
+
+	/**
 	 * Initialize the admin actions.
 	 */
 	public function __construct() {
 		add_action( 'add_meta_boxes', array( $this, 'register_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_meta_boxes' ) );
 	}
 
 	/**
@@ -48,7 +56,7 @@ class Camp_Certificates_Admin_Metaboxes {
 	 * @return string
 	 */
 	public function metabox_events( $post ) {
-
+		$this->_get_nonce_field();
 	}
 
 	/**
@@ -59,6 +67,71 @@ class Camp_Certificates_Admin_Metaboxes {
 	 * @return string
 	 */
 	public function metabox_attendees( $post ) {
+		$this->_get_nonce_field();
+	}
+
+	/**
+	 * Get WP nonce field.
+	 *
+	 * @return string
+	 */
+	private function _get_nonce_field() {
+		wp_nonce_field( basename( __FILE__ ), $this->nonce );
+	}
+
+	/**
+	 * Save meta boxes.
+	 *
+	 * @param  int $post_id
+	 *
+	 * @return void
+	 */
+	public function save_meta_boxes( $post_id ) {
+		// Verify nonce.
+		if ( ! isset( $_POST[ $this->nonce ] ) || ! wp_verify_nonce( $_POST[ $this->nonce ], basename( __FILE__ ) ) ) {
+			return;
+		}
+
+		// Verify if this is an auto save routine.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		// Check permissions.
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+		if ( isset( $_POST['post_type'] ) && 'cc_events' == $_POST['post_type'] ) {
+			$this->_save_events_data( $post_id, $_POST );
+		}
+
+		if ( isset( $_POST['post_type'] ) && 'cc_attendees' == $_POST['post_type'] ) {
+			$this->_save_attendees_data( $post_id, $_POST );
+		}
+	}
+
+	/**
+	 * Save events data.
+	 *
+	 * @param  int $post_id
+	 * @param  array $posted
+	 *
+	 * @return void
+	 */
+	private function _save_events_data( $post_id, $posted ) {
+
+	}
+
+	/**
+	 * Save events data.
+	 *
+	 * @param  int $post_id
+	 * @param  array $posted
+	 *
+	 * @return void
+	 */
+	private function _save_attendees_data( $post_id, $posted ) {
 
 	}
 }
